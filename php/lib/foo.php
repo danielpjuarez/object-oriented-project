b313bf4e61d264419cc34087854d5242913b01c4
Daniel Juarez <djuarez11@cnm.edu>
2019-07-16 16:46:53 -0600

    added what I sincerely hope is a properly formatted class creation

--- php/lib/foo.php
+++ php/lib/foo.php
@@ -1,1 +1,33 @@
 <?php
+/**	
+ * This table is an example of data collected and stored
+ * about an author for the purpose of categorizing them
+ */
+
+class author {
+	/**
+	 *id for this Author, primary key, obviously a required value
+	 **/
+	private $authorId;
+	/**
+	 *This is the URL for the author's avatar, whatever that is
+	 */
+	private $authorAvatarUrl;
+	/**
+	 * this is the author's activation token. In this context I don't know
+	 * what it is supposed to do
+	 */
+	private $authorActivationToken;
+	/**
+	 * this is the author's email, and it is required and unique
+	 */
+	private $authorEmail;
+	/**
+	 * this is the author's hash, and is required
+	 */
+	private $authorHash;
+	/**
+	 * this is the author's username, and is required and unique
+	 */
+	private $authorUsername;
+}
